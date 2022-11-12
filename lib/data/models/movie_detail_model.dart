61363aa9f9dd5e02803009894bab1677d08ffa91
Dhani AM <dhani.a.mm@gmail.com>
2022-11-12 20:44:00 +0800

    initial commit

---
+++ lib/data/models/movie_detail_model.dart
@@ -0,0 +1,145 @@
+import 'package:ditonton/data/models/genre_model.dart';
+import 'package:ditonton/domain/entities/movie_detail.dart';
+import 'package:equatable/equatable.dart';
+
+class MovieDetailResponse extends Equatable {
+  MovieDetailResponse({
+    required this.adult,
+    required this.backdropPath,
+    required this.genres,
+    required this.id,
+    required this.originalTitle,
+    required this.overview,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.runtime,
+    required this.title,
+    required this.voteAverage,
+    required this.voteCount,
+    required this.budget,
+    required this.homepage,
+    required this.imdbId,
+    required this.originalLanguage,
+    required this.popularity,
+    required this.revenue,
+    required this.status,
+    required this.tagline,
+    required this.video,
+  });
+
+  final bool adult;
+  final String? backdropPath;
+  final List<GenreModel> genres;
+  final int id;
+  final String originalTitle;
+  final String overview;
+  final String posterPath;
+  final String releaseDate;
+  final int runtime;
+  final String title;
+  final double voteAverage;
+  final int voteCount;
+  final int budget;
+  final String homepage;
+  final String? imdbId;
+  final String originalLanguage;
+  final double popularity;
+  final int revenue;
+  final String status;
+  final String tagline;
+  final bool video;
+
+  factory MovieDetailResponse.fromJson(Map<String, dynamic> json) =>
+      MovieDetailResponse(
+        adult: json["adult"],
+        backdropPath: json["backdrop_path"],
+        budget: json["budget"],
+        genres: List<GenreModel>.from(
+            json["genres"].map((x) => GenreModel.fromJson(x))),
+        homepage: json["homepage"],
+        id: json["id"],
+        imdbId: json["imdb_id"],
+        originalLanguage: json["original_language"],
+        originalTitle: json["original_title"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        releaseDate: json["release_date"],
+        revenue: json["revenue"],
+        runtime: json["runtime"],
+        status: json["status"],
+        tagline: json["tagline"],
+        title: json["title"],
+        video: json["video"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "adult": adult,
+        "backdrop_path": backdropPath,
+        "budget": budget,
+        "genres": List<dynamic>.from(genres.map((x) => x.toJson())),
+        "homepage": homepage,
+        "id": id,
+        "imdb_id": imdbId,
+        "original_language": originalLanguage,
+        "original_title": originalTitle,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "release_date": releaseDate,
+        "revenue": revenue,
+        "runtime": runtime,
+        "status": status,
+        "tagline": tagline,
+        "title": title,
+        "video": video,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+
+  /// Call [MovieDetail] from domain/entities/movie_detail
+  MovieDetail toEntity() {
+    return MovieDetail(
+      adult: this.adult,
+      backdropPath: this.backdropPath,
+      genres: this.genres.map((genre) => genre.toEntity()).toList(),
+      id: this.id,
+      originalTitle: this.originalTitle,
+      overview: this.overview,
+      posterPath: this.posterPath,
+      releaseDate: this.releaseDate,
+      runtime: this.runtime,
+      title: this.title,
+      voteAverage: this.voteAverage,
+      voteCount: this.voteCount,
+    );
+  }
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        adult,
+        backdropPath,
+        budget,
+        genres,
+        homepage,
+        id,
+        imdbId,
+        originalLanguage,
+        originalTitle,
+        overview,
+        popularity,
+        posterPath,
+        releaseDate,
+        revenue,
+        runtime,
+        status,
+        tagline,
+        title,
+        video,
+        voteAverage,
+        voteCount,
+      ];
+}
---
+++ lib/domain/repositories/movie_repository.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/data/repositories/movie_repository_impl.dart';
+import 'package:ditonton/domain/entities/movie.dart';
+import 'package:ditonton/domain/entities/movie_detail.dart';
+
+abstract class MovieRepository {
+  /// Call [MovieRepositoryImpl] from data/repositories/movie_repository_impl
+  Future<Either<Failure, List<Movie>>> getNowPlayingMovies();
+  Future<Either<Failure, List<Movie>>> getPopularMovies();
+  Future<Either<Failure, List<Movie>>> getTopRatedMovies();
+  Future<Either<Failure, MovieDetail>> getMovieDetail(int id);
+  Future<Either<Failure, List<Movie>>> getMovieRecommendations(int id);
+  Future<Either<Failure, List<Movie>>> searchMovies(String query);
+  Future<Either<Failure, String>> saveWatchlist(MovieDetail movie);
+  Future<Either<Failure, String>> removeWatchlist(MovieDetail movie);
+  Future<bool> isAddedToWatchlist(int id);
+  Future<Either<Failure, List<Movie>>> getWatchlistMovies();
+}

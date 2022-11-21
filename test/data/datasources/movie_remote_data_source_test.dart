36d8ce8e386d82d70daf909f5fa27ad653324fea
Dhani AM <dhani.a.mm@gmail.com>
2022-11-21 12:24:32 +0800

    added tv_series_recommendation.json

--- test/data/datasources/movie_remote_data_source_test.dart
+++ test/data/datasources/movie_remote_data_source_test.dart
@@ -25,7 +25,7 @@ void main() {
 
   group('get Now Playing Movies', () {
     final tMovieList = MovieResponse.fromJson(
-            json.decode(readJson('dummy_data/now_playing.json')))
+            json.decode(readJson('dummy_data/now_playing_movie.json')))
         .movieList;
 
     test('should return list of Movie Model when the response code is 200',
@@ -34,7 +34,7 @@ void main() {
       when(mockHttpClient
               .get(Uri.parse('$BASE_URL/movie/now_playing?$API_KEY')))
           .thenAnswer((_) async =>
-              http.Response(readJson('dummy_data/now_playing.json'), 200));
+              http.Response(readJson('dummy_data/now_playing_movie.json'), 200));
       // act
       final result = await dataSource.getNowPlayingMovies();
       // assert

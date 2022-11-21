f23287e03470ea38161a36a012f79fd2ede4ff8e
Dhani AM <dhani.a.mm@gmail.com>
2022-11-21 12:21:17 +0800

    added now_playing_tv_series.json

--- test/data/datasources/tv_series_remote_data_source_test.dart
+++ test/data/datasources/tv_series_remote_data_source_test.dart
@@ -1,4 +1,19 @@
+import 'package:ditonton/data/datasources/tv_series_remote_data_source.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
 void main() {
   const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
   const BASE_URL = 'https://api.themoviedb.org/3';
+
+  late TvSeriesRemoteDataSourceImpl dataSource;
+  late MockHttpClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockHttpClient();
+    dataSource = TvSeriesRemoteDataSourceImpl(client: mockHttpClient);
+  });
+
+  group('get Now Playing Tv Series', () {});
 }

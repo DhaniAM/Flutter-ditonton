9654a64ac177d71f24615c792d7c5fb6b87357ae
Dhani AM <dhani.a.mm@gmail.com>
2022-11-21 12:05:48 +0800

    updated gradle

--- test/data/datasources/movie_remote_data_source_test.dart
+++ test/data/datasources/movie_remote_data_source_test.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
+import 'package:ditonton/common/exception.dart';
 import 'package:ditonton/data/datasources/movie_remote_data_source.dart';
 import 'package:ditonton/data/models/movie_detail_model.dart';
 import 'package:ditonton/data/models/movie_response.dart';
-import 'package:ditonton/common/exception.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
 
-import '../../json_reader.dart';
 import '../../helpers/test_helper.mocks.dart';
+import '../../json_reader.dart';
 
 void main() {
   const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
---
+++ test/data/datasources/tv_series_remote_data_source_test.dart
@@ -0,0 +1,4 @@
+void main() {
+  const API_KEY = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
+  const BASE_URL = 'https://api.themoviedb.org/3';
+}

2568674b244bf4287e656e14302d33eff7be566c
Dhani AM <dhani.a.mm@gmail.com>
2022-11-27 13:09:39 +0800

    added usecases test implementations

---
+++ test/domain/usecases/get_popular_tv_series_test.dart
@@ -0,0 +1,35 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/get_popular_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetPopularTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRpository;
+
+  setUp(() {
+    mockTvSeriesRpository = MockTvSeriesRepository();
+    usecase = GetPopularTvSeries(mockTvSeriesRpository);
+  });
+
+  final tTvSeries = <TvSeries>[];
+
+  group('GetPopularTvSeries Tests', () {
+    group('execute', () {
+      test(
+          'should get list of tv series from the repository when execute function is called',
+          () async {
+        // arrange
+        when(mockTvSeriesRpository.getPopularTvSeries())
+            .thenAnswer((_) async => Right(tTvSeries));
+        // act
+        final result = await usecase.execute();
+        // assert
+        expect(result, Right(tTvSeries));
+      });
+    });
+  });
+}
---
+++ test/domain/usecases/get_top_rated_tv_series_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/get_top_rated_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late GetTopRatedTvSeries usecase;
+  late MockTvSeriesRepository mockTvSeriesRepository;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = GetTopRatedTvSeries(mockTvSeriesRepository);
+  });
+
+  final tTvSeries = <TvSeries>[];
+
+  test('should get list of tv series from repository', () async {
+    // arrange
+    when(mockTvSeriesRepository.getTopRatedTvSeries())
+        .thenAnswer((_) async => Right(tTvSeries));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(tTvSeries));
+  });
+}

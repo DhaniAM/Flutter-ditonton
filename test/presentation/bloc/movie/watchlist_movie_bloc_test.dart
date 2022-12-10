f864ff93028b5b69a51966fe4ffbc51ba06a8b9c
Dhani AM <dhani.a.mm@gmail.com>
2022-12-11 00:52:31 +0800

    added watchlist movies  bloc test

--- test/presentation/bloc/movie/watchlist_movie_bloc_test.dart
+++ test/presentation/bloc/movie/watchlist_movie_bloc_test.dart
@@ -1,5 +1,57 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/usecases/get_watchlist_movies.dart';
+import 'package:ditonton/presentation/bloc/movie/watchlist_movie_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/watchlist_movie_event.dart';
+import 'package:ditonton/presentation/bloc/movie/watchlist_movie_state.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import 'watchlist_movie_bloc_test.mocks.dart';
 
 @GenerateMocks([GetWatchlistMovies])
-void main() {}
+void main() {
+  late MockGetWatchlistMovies mockGetWatchlistMovies;
+  late WatchlistMoviesBloc watchlistMoviesBloc;
+
+  setUp(() {
+    mockGetWatchlistMovies = MockGetWatchlistMovies();
+    watchlistMoviesBloc = WatchlistMoviesBloc(mockGetWatchlistMovies);
+  });
+
+  blocTest<WatchlistMoviesBloc, WatchlistMoviesState>(
+    'Should emit [Loading, HasData] when WatchlistMovies data is success',
+    build: () {
+      when(mockGetWatchlistMovies.execute())
+          .thenAnswer((_) async => Right(testMovieList));
+      return watchlistMoviesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchWatchlistMovies()),
+    expect: () => [
+      WatchlistMoviesLoading(),
+      WatchlistMoviesHasData(testMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetWatchlistMovies.execute());
+    },
+  );
+  blocTest<WatchlistMoviesBloc, WatchlistMoviesState>(
+    'Should emit [Loading, HasData] when WatchlistMovies data is failed',
+    build: () {
+      when(mockGetWatchlistMovies.execute())
+          .thenAnswer((_) async => Left(ServerFailure('')));
+      return watchlistMoviesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchWatchlistMovies()),
+    expect: () => [
+      WatchlistMoviesLoading(),
+      WatchlistMoviesError(''),
+    ],
+    verify: (bloc) {
+      verify(mockGetWatchlistMovies.execute());
+    },
+  );
+}

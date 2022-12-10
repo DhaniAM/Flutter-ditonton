3b3617e18b8799a9d63ea97df38b4ea353eab7ed
Dhani AM <dhani.a.mm@gmail.com>
2022-12-11 00:04:14 +0800

    added top rated movie bloc test

--- test/presentation/bloc/movie/top_rated_movies_bloc_test.dart
+++ test/presentation/bloc/movie/top_rated_movies_bloc_test.dart
@@ -1,5 +1,58 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/usecases/get_top_rated_movies.dart';
+import 'package:ditonton/presentation/bloc/movie/top_rated_movies_bloc.dart';
+import 'package:ditonton/presentation/bloc/movie/top_rated_movies_event.dart';
+import 'package:ditonton/presentation/bloc/movie/top_rated_movies_state.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import 'top_rated_movies_bloc_test.mocks.dart';
 
 @GenerateMocks([GetTopRatedMovies])
-void main() {}
+void main() {
+  late MockGetTopRatedMovies mockGetTopRatedMovies;
+  late TopRatedMoviesBloc topRatedMoviesBloc;
+
+  setUp(() {
+    mockGetTopRatedMovies = MockGetTopRatedMovies();
+    topRatedMoviesBloc = TopRatedMoviesBloc(mockGetTopRatedMovies);
+  });
+
+  blocTest<TopRatedMoviesBloc, TopRatedMoviesState>(
+    'Should emit [Loading, HasData] when TopRatedMovie data is success ',
+    build: () {
+      when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Right(testMovieList));
+      return topRatedMoviesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchTopRatedMovies()),
+    expect: () => [
+      TopRatedMoviesLoading(),
+      TopRatedMoviesHasData(testMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+
+  blocTest<TopRatedMoviesBloc, TopRatedMoviesState>(
+    'Should emit [Loading, Error] when TopRatedMovie data is failed ',
+    build: () {
+      when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Left(ServerFailure('')));
+      return topRatedMoviesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchTopRatedMovies()),
+    expect: () => [
+      TopRatedMoviesLoading(),
+      TopRatedMoviesError(''),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+}

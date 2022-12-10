af6ccf371e7487be9ed75937d8775312c0606390
Dhani AM <dhani.a.mm@gmail.com>
2022-12-11 00:06:04 +0800

    added top rated tv series bloc test

--- test/presentation/bloc/tv_series/top_rated_tv_series_bloc_test.dart
+++ test/presentation/bloc/tv_series/top_rated_tv_series_bloc_test.dart
@@ -1,5 +1,58 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
 import 'package:ditonton/domain/usecases/get_top_rated_tv_series.dart';
+import 'package:ditonton/presentation/bloc/tv_series/top_rated_tv_series_bloc.dart';
+import 'package:ditonton/presentation/bloc/tv_series/top_rated_tv_series_event.dart';
+import 'package:ditonton/presentation/bloc/tv_series/top_rated_tv_series_state.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../dummy_data/dummy_objects.dart';
+import 'top_rated_tv_series_bloc_test.mocks.dart';
 
 @GenerateMocks([GetTopRatedTvSeries])
-void main() {}
+void main() {
+  late MockGetTopRatedTvSeries mockGetTopRatedTvSeries;
+  late TopRatedTvSeriesBloc topRatedTvSeriesBloc;
+
+  setUp(() {
+    mockGetTopRatedTvSeries = MockGetTopRatedTvSeries();
+    topRatedTvSeriesBloc = TopRatedTvSeriesBloc(mockGetTopRatedTvSeries);
+  });
+
+  blocTest<TopRatedTvSeriesBloc, TopRatedTvSeriesState>(
+    'Should emit [Loading, HasData] when TopRatedTvSeries data is success ',
+    build: () {
+      when(mockGetTopRatedTvSeries.execute())
+          .thenAnswer((_) async => Right(testTvSeriesList));
+      return topRatedTvSeriesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchTopRatedTvSeries()),
+    expect: () => [
+      TopRatedTvSeriesLoading(),
+      TopRatedTvSeriesHasData(testTvSeriesList),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedTvSeries.execute());
+    },
+  );
+
+  blocTest<TopRatedTvSeriesBloc, TopRatedTvSeriesState>(
+    'Should emit [Loading, Error] when TopRatedTvSeries data is failed ',
+    build: () {
+      when(mockGetTopRatedTvSeries.execute())
+          .thenAnswer((_) async => Left(ServerFailure('')));
+      return topRatedTvSeriesBloc;
+    },
+    act: (bloc) => bloc.add(OnFetchTopRatedTvSeries()),
+    expect: () => [
+      TopRatedTvSeriesLoading(),
+      TopRatedTvSeriesError(''),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedTvSeries.execute());
+    },
+  );
+}
